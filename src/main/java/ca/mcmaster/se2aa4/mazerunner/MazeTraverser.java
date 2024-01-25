c4da4c65ce47433773a02f1ad918512de623a3c6
aaronloh16 <aaronloh04@gmail.com>
2024-01-24 22:23:49 -0500

    skeleton for right hand algorithm

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeTraverser.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeTraverser.java
@@ -1,42 +1,50 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 public class MazeTraverser {
-    public String traverseMaze(char[][] maze) {
-        // ASSUMES the maze is a straight horizontal line with walls on top and bottom.
 
-        int pathRow = findPathRow(maze);
-        if (pathRow == -1) {
-            return "No path found in the maze";
-        }
+    public String traverseMaze(char[][] maze) {
+        // Initialize position and direction(needs implementing), so far assuming east to west traveral only
+        int x = findStartX(maze);
+        int y = 0;
+        Direction direction = Direction.EAST;
 
         StringBuilder path = new StringBuilder();
 
-
-        for (int col = 0; col < maze[0].length - 1; col++) {
-            if (maze[pathRow][col] == ' ') {
+        while (maze[x][y] != 'E') { // Assuming 'E' marks the exit, will need to configure maze better so this can be recognized
+            if (canTurnRight(x, y, direction, maze)) {
+                direction = turnRight(direction);
+                path.append('R');
+            } else if (canMoveForward(x, y, direction, maze)) {
+                moveForward(direction);
                 path.append('F');
             } else {
-                return "Encountered an obstacle";
+                direction = turnLeft(direction);
+                path.append('L');
             }
         }
 
         return path.toString();
+
+        //need to implement the turn and move helper methods, just skeleton for now
+        //also need to track direction somehow
     }
 
 
 
 
-        public boolean verifyPath(char[][] maze, String path) {
-
-            long forwardMoves = path.chars().filter(ch -> ch == 'F').count(); //count forward moves in path
-
-            int mazeWidth = maze[0].length;
-
-            // Verify if the number of forward moves equals the maze width minus 1, very MVP
-            return forwardMoves == (mazeWidth - 1);
-        }
 
 
+
+
+    public boolean verifyPath(char[][] maze, String path) {
+
+        long forwardMoves = path.chars().filter(ch -> ch == 'F').count(); //count forward moves in path
+
+        int mazeWidth = maze[0].length;
+
+        // Verify if the number of forward moves equals the maze width minus 1, very MVP
+        return forwardMoves == (mazeWidth - 1);
+    }
 
 
     private int findPathRow(char[][] maze) {
@@ -49,5 +57,7 @@ public class MazeTraverser {
         }
         return -1;
     }
+
+
 }
 
