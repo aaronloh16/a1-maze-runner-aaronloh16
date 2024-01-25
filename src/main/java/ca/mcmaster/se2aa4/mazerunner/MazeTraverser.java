4dffe98f3cfdba496bd41e9f1b248e01459a61a1
aaronloh16 <aaronloh04@gmail.com>
2024-01-25 18:08:01 -0500

    feat:added required move forward  method and various spacial awareness checks, fixed traverser logic

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeTraverser.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeTraverser.java
@@ -28,33 +28,111 @@ public class MazeTraverser {
     }
 
 
+    private int[] getAdjacentCoordinates(int x, int y, Direction direction) {
+        return switch (direction) {
+            case NORTH -> new int[]{x - 1, y};
+            case EAST -> new int[]{x, y + 1};
+            case SOUTH -> new int[]{x + 1, y};
+            case WEST -> new int[]{x, y - 1};
+        };
+    }
+
+    private boolean isValidMove(int x, int y, char[][] maze) {
+        return x >= 0 && x < maze.length && y >= 0 && y < maze[0].length && maze[x][y] == ' ';
+    }
+
+    private boolean canTurnRight(int x, int y, Direction direction, char[][] maze) {
+        int[] rightCoordinates = getAdjacentCoordinates(x, y, direction.turnRight());
+        return isValidMove(rightCoordinates[0], rightCoordinates[1], maze);
+    }
+
+    private boolean canTurnLeft(int x, int y, Direction direction, char[][] maze) {
+        int[] leftCoordinates = getAdjacentCoordinates(x, y, direction.turnLeft());
+        return isValidMove(leftCoordinates[0], leftCoordinates[1], maze);
+    }
+
+    private boolean canMoveForward(int x, int y, Direction direction, char[][] maze) {
+        int[] forwardCoordinates = getAdjacentCoordinates(x, y, direction);
+        return isValidMove(forwardCoordinates[0], forwardCoordinates[1], maze);
+    }
+
+    private int[] moveForward(int x, int y, Direction direction) {
+        return getAdjacentCoordinates(x, y, direction);
+    }
+
+    private int findStartX(char[][] maze) {  //used to be findPathRow in MVP
+        for (int x = 0; x < maze.length; x++) {
+            if (maze[x][0] == ' ') {
+                return x;
+            }
+        }
+        return -1; // if its not found
+    }
+
+    private int[] findExitCoordinates(char[][] maze) {
+        for (int x = 0; x < maze.length; x++) {
+            if (maze[x][maze[0].length - 1] == ' ') { //same as find entrance but for west side
+                return new int[]{x, maze[0].length - 1};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+
+
+
+
     public String traverseMaze(char[][] maze) {
-        // Initialize position and direction(needs implementing), so far assuming east to west traveral only
+        int[] exitCoordinates = findExitCoordinates(maze);
+        if (exitCoordinates[0] == -1 || exitCoordinates[1] == -1) {
+            return "Exit not found";
+        }
+
         int x = findStartX(maze);
         int y = 0;
         Direction direction = Direction.EAST;
 
+        if (x == -1) {
+            return "Entrance not found";
+        }
+
         StringBuilder path = new StringBuilder();
 
-        while (maze[x][y] != 'E') { // Assuming 'E' marks the exit, will need to configure maze better so this can be recognized
+        int iterationCount = 0;
+        int limit = 1000; // Set a limit to prevent infinite loops
+
+        while ((x != exitCoordinates[0] || y != exitCoordinates[1]) && iterationCount < limit) {
+            System.out.println("Step " + iterationCount + ": Position (" + x + ", " + y + "), Direction: " + direction);
+
             if (canTurnRight(x, y, direction, maze)) {
-                direction = turnRight(direction);
+                direction = direction.turnRight();
                 path.append('R');
-            } else if (canMoveForward(x, y, direction, maze)) {
-                moveForward(direction);
+                int[] newCoordinates = moveForward(x, y, direction);
+                x = newCoordinates[0];
+                y = newCoordinates[1];
                 path.append('F');
-            } else {
-                direction = turnLeft(direction);
+            } else if (canMoveForward(x, y, direction, maze)) {
+                int[] newCoordinates = moveForward(x, y, direction);
+                x = newCoordinates[0];
+                y = newCoordinates[1];
+                path.append('F');
+            } else if (canTurnLeft(x, y, direction, maze)) { // Check if can turn left
+                direction = direction.turnLeft();
                 path.append('L');
+            } else {
+                // Dead-end, turn around
+                direction = direction.turnLeft().turnLeft();
+                path.append('L').append('L');
             }
+            iterationCount++;
+        }
+
+        if (iterationCount >= limit) {
+            return "possible infinite loop";
         }
 
         return path.toString();
-
-        //need to implement the turn and move helper methods, just skeleton for now
-        //also need to track direction somehow
     }
-
 
 
 
@@ -73,16 +151,7 @@ public class MazeTraverser {
     }
 
 
-    private int findPathRow(char[][] maze) {
-        for (int row = 0; row < maze.length; row++) {
-            // Check if the first and last columns in this row are not walls,
-            // indicating that this row contains the path
-            if (maze[row][0] == ' ') {
-                return row;
-            }
-        }
-        return -1;
-    }
+
 
 
 }
